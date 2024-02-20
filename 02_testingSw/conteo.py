5ff9a13b68b127d90f897cdc04ef597f394bafcf
davidAtauri <56089072+davidAtauri@users.noreply.github.com>
2024-02-20 16:45:11 +0100

    testingSw v1.0 ready

--- 02_testingSw/conteo.py
+++ 02_testingSw/conteo.py
@@ -18,39 +18,43 @@ def allow_cors(func):
         return func(*args, **kwargs)
     return wrapper
 
-'''Let buffer between 0..127 if its in range 0..1'''
-'''Some old captures are in the range 0..1'''
+'''Let buffer between 0..127 if its in range 0..1
+Some old captures are in the range 0..1'''
 def desnormalizar(buff):
     if (max(buff['interior'])<=1):
         buff['interior'] = [int(i*127) for i in buff['interior']]
         buff['exterior'] = [int(i*127) for i in buff['exterior']]
     return buff
 
-def getBufferFromUrlRequest(rq):
-    
+'''buffer gets raw data from counter electrodes (only one escape!)'''
+def getBufferFromUrlRequest(rq):    
     data = False
     try:
         bufferUrl = rq.POST['buffer']
         r = requests.get(bufferUrl)
         data = desnormalizar(json.loads(r.text))
-        #print(data)
     except Exception as e: print(e)
     return data
 
-#===========================================================
-# busca los picos con libreria scipy
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks.html
+# COUNTING ALGORITHM'S TO TRY...
+# THEY RECEIVE A BUFFER OF RAW DATA 
+'''{
+    'interior':[...],
+    'exterior':[...]
+}'''
+#return an array of ints with same langth as buffer with '1' whwere a bee is detected:
+'''{
+    'bees':[0,0,0,0,0,1,0,0....]
+}'''
 
-@post('/peaks')
+# Search for peaks buffer['exterior'] whit scipy library (Just demo)
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks.html
 @allow_cors
-def peaks():
+def peaks(buffer):
     from scipy.signal import find_peaks
     import numpy as np
-   
-    buffer = getBufferFromUrlRequest(request)  
+
     bees=[0] * len(buffer['interior']) 
-    
-    print("Voy con todo!")
     
     # sensor interior
     '''peaks, _ = find_peaks(buffer['interior'], height=50, distance=100)
@@ -67,6 +71,33 @@ def peaks():
     }
     #print(r)
     return json.dumps(r)
+
+@allow_cors
+def myCountingAlgorithm(buffer):
+   
+    bees=[0] * len(buffer['interior']) 
+    
+    ''' 
+    
+     
+           YOUR CODE HERE
+
+           
+     
+    '''
+    r={
+        "bees": bees
+    }
+    return json.dumps(r) # make a string
+
+'''Called from testingSw/front ---> countBees button'''
+@post('/countBees')
+@allow_cors
+def countBees():
+    buffer = getBufferFromUrlRequest(request)  # get data
+    
+    return peaks(buffer) # Comment to try your own algorithm
+    return myCountingAlgorithm(buffer) # Uncomment to try your own algorithm
 
 run(host='0.0.0.0', server="paste", port=8008)
 
