20f5320e1e50a652cae0198fb92fbf7abf7bc20f
atauri <david.atauri@gmail.com>
2024-07-23 14:13:01 +0200

    con camara

--- 03_rpiVersion/gdansk/splendid/pintar.py
+++ 03_rpiVersion/gdansk/splendid/pintar.py
@@ -4,27 +4,27 @@ import matplotlib.animation as animation
 import random
 import time
 import threading
-
+from scipy.signal import savgol_filter
+        
 import socket
 import sys
 from struct import unpack
 
+from webcam import Webcam
+
 # Create figure for plotting
 fig = plt.figure()
-
 ax = fig.add_subplot(1, 1, 1)
-xs = [0]*2000
+
 ys = [0]*2000
-i=0
 
 # Aquí leer por el socket del contador --------------
 def soc():
 
-
     print("\nCrear Socket")
 
     global ys
-
+    global lock
 
     # crear el socket (servidor)
     # Tengo que saber mi IP (y ponérsela fija en e el router)
@@ -46,33 +46,56 @@ def soc():
         try: 
             x  = unpack('f', message)
             #print(f'X: {x}')
+            lock.acquire()
             ys.append(x)
-        except:pass
+            ys.pop(0)
+            lock.release()
+        except Exception as e : print(e)
         #time.sleep(.01)
 
 def animate(i):
 
     global ys
+    global lock
 
-    try: 
-        ys=ys[-2000:]
-        ax.clear()
-        ax.plot(ys, linewidth=1)
+    ax.clear()
+    lock.acquire()
+    y = ys.copy()
+    try:  
+        suave = savgol_filter(y, window_length=50, polyorder=2) 
 
-        # Format plot
-        #plt.xticks(rotation=45, ha='right')
-        plt.subplots_adjust(bottom=0.30)
-        plt.title('RT')
-        plt.ylim(0,1)
-
-
-    except: print("error")
-
+    except Exception as e: print("SUAVIZAR:",e)
+    try:        
+        ax.plot(y, color="black", linewidth=.75)
+    except Exception as e: print("PINTAR:", e)
+    lock.release()
     
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('Splendid escape')
+    plt.ylim(0,1)
+    
+def getVideo():
+    
+    import cv2
+    capture = cv2.VideoCapture(1)
+ 
+    while (capture.isOpened()):
+        ret, frame = capture.read()
+        cv2.imshow('webCam',frame)
+        if (cv2.waitKey(1) == ord('s')):
+            break
+    
+    capture.release()
+    cv2.destroyAllWindows()
+
 
 # Set up plot to call animate() function periodically
-ani = animation.FuncAnimation(fig, animate, interval=100)
-hilo = threading.Thread(target=soc, name='hilo4').start()
+lock = threading.Lock()
+
+ani = animation.FuncAnimation(fig, animate, interval=250)
+data = threading.Thread(target=soc).start()
+video = threading.Thread(target=getVideo).start()
+
 
 plt.show()
 
